3e202183b00cb688a5aa3e7eca29eda59c35dfb7
xDhairyax <66120130+xDhairyax@users.noreply.github.com>
2021-08-26 17:46:45 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import plotly.express as px
+
+df=pd.read_csv("data.csv")
+
+fig=px.scatter(df,x="date",y="cases",color="country",size_max=60)
+fig.show()
---
+++ project.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+data=['a','b','c','d','e']
+
+df=pd.DataFrame(data)
+print(df)
